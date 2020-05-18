727c2843ecbe1d680d487fbd63a15cfb98b24f4f
GracieleRibeiro <graciele.ribeiro@hotmail.com>
2020-05-18 19:28:53 -0300

    Usando redux e adicionando imagem

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux'
+import reduxThunk from 'redux-thunk';
+import UsuariosNavegacao from './navegacao/UsuariosNavegacao';
+import usuariosReducer from './store/usuarios-reducer'
+
+const rootReducer = combineReducers({
+  usuarios: usuariosReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+
+export default function App() {
+  return <Provider store={store}>
+        <UsuariosNavegacao />
+        </Provider>
+}
